cf96cfe411424ef39f1fe5b10ead69c33dbe74f0
Anastasiia Riadnova <a.ryadnova01@gmail.com>
2026-04-16 18:36:09 +0200

    add task solution v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write your code here
+function showNotification(type, message) {
+  const notification = document.createElement('div');
+
+  // eslint-disable-next-line max-len
+  notification.className =
+    'message' + (type === 'error' ? ' error-message' : '');
+  notification.textContent = message;
+  document.body.appendChild(notification);
+}
+
+const firstPromise = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  if (logo) {
+    logo.addEventListener('click', (eventy) => {
+      if (eventy.button === 0) {
+        resolve('Promise was resolved!');
+      }
+    });
+  }
+});
+
+firstPromise
+  .then((message) => showNotification('success', message))
+  .catch((err) => showNotification('error', err.message));
+
+const secondPromise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error('Promise was rejected!')), 3000);
+});
+
+secondPromise
+  .then((message) => showNotification('success', message))
+  .catch((err) => showNotification('error', err.message));
